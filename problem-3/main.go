9e47492730b6af6fbde2c5c94d3ea3ffa4abcac0
RamaniVadla-1318 <ramanivadla74@gmail.com>
2025-04-13 13:26:18 +0530

    Update main.go

--- problem-3/main.go
+++ problem-3/main.go
@@ -1,1 +1,19 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "time"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    currentTime := time.Now().Format("Monday, 02-Jan-2006 15:04:05 MST")
+    fmt.Fprintf(w, "<h1>Current Date and Time</h1><p>%s</p>", currentTime)
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    fmt.Println("Starting server on :8080...")
+    http.ListenAndServe(":8080", nil)
+}
 
